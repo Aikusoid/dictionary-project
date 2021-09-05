d0eb8d0e544958e32ab2f50c35f7ef997bbba6ed
Aigerim <aigerim.adrysheva@gmail.com>
2021-09-05 15:33:18 +0200

    Update Phonetics.js

--- src/components/Phonetics.js
+++ src/components/Phonetics.js
@@ -1,20 +1,25 @@
-import React from "react"
+import React from "react";
 
-export default function Phonetics(props){
-    if(props.phonetics){
-        return(
-            <div className="Phonetics">
-                {props.phonetics.map((phonetic, index)=>{
-                    return(
-                        <p key={index} >
-                        <a className="btn" href={phonetic.audio} target="_blank" rel="noreferrer">
-                            <i className="fas fa-volume-up"></i>
-                        </a>
-                        {phonetic.text}
-                        </p>
-                    );
-                })}
-            </div>
-        )
-    }
+export default function Phonetics(props) {
+  if (props.phonetics) {
+    return (
+      <div className="Phonetics">
+        {props.phonetics.map((phonetic, index) => {
+          return (
+            <p key={index}>
+              <a
+                className="btn"
+                href={phonetic.audio}
+                target="_blank"
+                rel="noreferrer"
+              >
+                <i className="fas fa-volume-up"></i>
+              </a>
+              {phonetic.text}
+            </p>
+          );
+        })}
+      </div>
+    );
+  }
 }
